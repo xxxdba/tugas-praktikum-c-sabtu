2ac6fb9ecda107d2607bab802b2d6d28c5e26691
xxxdba <devdesign.db@gmail.com>
2024-10-24 18:49:45 +0700

    first commit

---
+++ tugas-4/tugas mandiri 4 nomor 1.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    
+    // Input nilai N
+    printf("Masukkan nilai N: ");
+    scanf("%d", &N);
+    
+    // Proses berdasarkan kondisi N > 50
+    if (N > 50) {
+        N = N + 10;
+    } else {
+        N = N - 25;
+    }
+    
+    // Output hasil akhir N
+    printf("Nilai N: %d\n", N);
+    
+    return 0;
+}
+
